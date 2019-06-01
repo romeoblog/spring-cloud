fcb6cc74ef609a3951d16c497760b81ab7b23c5e
zheng_weilu@163.com <zheng_weilu@163.com>
2019-06-01 16:17:12 +0800

    add @GlobalTransactional

--- example-seata/business-service/src/main/java/com/example/cloud/service/BusinessService.java
+++ example-seata/business-service/src/main/java/com/example/cloud/service/BusinessService.java
@@ -2,6 +2,7 @@ package com.example.cloud.service;
 
 import com.example.cloud.feign.OrderFeignClient;
 import com.example.cloud.feign.StorageFeignClient;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class BusinessService {
     @Autowired
     private OrderFeignClient orderFeignClient;
 
-    //@GlobalTransactional
+    @GlobalTransactional
     public void purchase(String userId, String commodityCode, int orderCount) {
         storageFeignClient.deduct(commodityCode, orderCount);
 
