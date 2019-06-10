66e06312e1bf043b7eda789d09fdd6b95c7a0042
zheng_weilu@163.com <zheng_weilu@163.com>
2019-06-10 10:03:29 +0800

    Upgrade pom.xml

---
+++ example-platform/src/main/java/com/cloud/example/platform/aop/annotation/UnPermission.java
@@ -0,0 +1,14 @@
+package com.cloud.example.platform.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface UnPermission {
+
+    boolean value() default true;
+
+}
