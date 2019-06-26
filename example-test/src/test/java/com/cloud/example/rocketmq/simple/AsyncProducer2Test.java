8b989bc34be549b22c2c362fc00517d515e8ff81
zheng_weilu@163.com <zheng_weilu@163.com>
2019-06-26 15:05:37 +0800

    Add Class with AsyncProducer2Test

---
+++ example-test/src/test/java/com/cloud/example/rocketmq/simple/AsyncProducer2Test.java
@@ -0,0 +1,78 @@
+/*
+ *  Copyright 2019 https://github.com/romeoblog/spring-cloud.git Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.cloud.example.rocketmq.simple;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Send Messages Asynchronously
+ * <p>
+ * Asynchronous transmission is generally used in response time sensitive business scenarios.
+ * </p>
+ *
+ * @author Benji
+ * @date 2019-06-26
+ */
+public class AsyncProducer2Test {
+
+    public static void main(String[] args) throws MQClientException, InterruptedException, UnsupportedEncodingException {
+
+        DefaultMQProducer producer = new DefaultMQProducer("Jodie_Daily_test");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+
+        int messageCount = 10;
+        final CountDownLatch countDownLatch = new CountDownLatch(messageCount);
+        for (int i = 0; i < messageCount; i++) {
+            try {
+                final int index = i;
+                Message msg = new Message("TopicTest",
+                        "TagA",
+                        "OrderID188",
+                        "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+                producer.send(msg, new SendCallback() {
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d OK %s %n", index, sendResult.getMsgId());
+                    }
+
+                    @Override
+                    public void onException(Throwable e) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d Exception %s %n", index, e);
+                        e.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        countDownLatch.await(5, TimeUnit.SECONDS);
+        producer.shutdown();
+    }
+
+}
