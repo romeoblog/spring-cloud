6286a9b8691556b20e086a40f1af0ac10257b1f1
zheng_weilu@163.com <zheng_weilu@163.com>
2019-07-04 17:49:15 +0800

    add method: ProviderService

--- example-mq/example-rocketmq-producer/src/main/java/com/cloud/example/mq/rocketmq/producer/service/ProviderService.java
+++ example-mq/example-rocketmq-producer/src/main/java/com/cloud/example/mq/rocketmq/producer/service/ProviderService.java
@@ -15,10 +15,17 @@
  */
 package com.cloud.example.mq.rocketmq.producer.service;
 
+import org.apache.rocketmq.common.message.MessageConst;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 消息生产者服务
@@ -34,6 +41,21 @@ public class ProviderService {
 
     public void send(String message) {
         output.send(MessageBuilder.withPayload(message).build());
+    }
+
+    public <T> void sendWithTags(T msg, String tag) {
+        Message message = MessageBuilder.createMessage(msg,
+                new MessageHeaders(Stream.of(tag).collect(Collectors
+                        .toMap(str -> MessageConst.PROPERTY_TAGS, String::toString))));
+        output.send(message);
+    }
+
+    public <T> void sendObject(T msg, String tag) {
+        Message message = MessageBuilder.withPayload(msg)
+                .setHeader(MessageConst.PROPERTY_TAGS, tag)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build();
+        output.send(message);
     }
 
 }
