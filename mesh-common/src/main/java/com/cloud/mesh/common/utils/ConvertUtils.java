d2663998e3067c58fc580d093f7940c055023737
zheng_weilu@163.com <zheng_weilu@163.com>
2019-09-21 21:53:32 +0800

    Add Copyright information

--- mesh-common/src/main/java/com/cloud/mesh/common/utils/ConvertUtils.java
+++ mesh-common/src/main/java/com/cloud/mesh/common/utils/ConvertUtils.java
@@ -1,9 +1,17 @@
-/**
- * Copyright (c) 2005-2010 springside.org.cn
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * <p>
- * $Id: ConvertUtils.java 1211 2010-09-10 16:20:45Z calvinxiu $
+/*
+ *  Copyright 2019 https://github.com/romeoblog/spring-cloud.git Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.cloud.mesh.common.utils;
 
