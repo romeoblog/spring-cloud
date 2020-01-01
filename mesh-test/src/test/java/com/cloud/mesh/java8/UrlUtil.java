e0b1801e153d57b0d99adad686d32a5824426cef
zheng_weilu@163.com <zheng_weilu@163.com>
2020-01-01 11:46:54 +0800

    Happy New Year

--- mesh-test/src/test/java/com/cloud/mesh/java8/UrlUtil.java
+++ mesh-test/src/test/java/com/cloud/mesh/java8/UrlUtil.java
@@ -1,10 +1,29 @@
+/*
+ *  Copyright 2019 https://github.com/romeoblog/spring-cloud.git Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.cloud.mesh.java8;
 
 import com.cloud.mesh.common.utils.StringUtils;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class UrlUtil {
 
@@ -68,5 +87,13 @@ public class UrlUtil {
 		System.out.println(parse(s, null));
 		System.out.println(parse(key, null));
 //        System.out.println(getParam(key, "ss"));
+
+        List<String> list = Lists.newArrayList();
+        list.add("2");
+
+        list = list.stream().filter(ss -> !Objects.equals(ss, "2")).collect(Collectors.toList());
+
+        System.out.println(list);
+
 	}
 }
