3383b6fc1cd9cf8e154e8ea16c671d54444da979
willlu.zheng <willluzheng@163.com>
2020-06-03 13:54:20 +0800

    Add ConfigApplication Clz

--- mesh-config/src/main/java/com/cloud/mesh/config/ConfigApplication.java
+++ mesh-config/src/main/java/com/cloud/mesh/config/ConfigApplication.java
@@ -1,13 +1,27 @@
 package com.cloud.mesh.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 /**
  * spring cloud config
  *
  * @author willlu.zheng
  * @date 2020-06-03
  */
+@SpringBootApplication
+@EnableConfigServer
+@RefreshScope
+@Slf4j
 public class ConfigApplication {
     public static void main(String[] args) {
-
+        SpringApplication app = new SpringApplication(ConfigApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+        log.info("BusinessApplication is running!");
     }
 }
