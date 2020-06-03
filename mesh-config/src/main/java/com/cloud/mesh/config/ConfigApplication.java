464c07f7afd1324644c52de72df7288ef36be3c1
willlu.zheng <willluzheng@163.com>
2020-06-03 13:54:42 +0800

    Add ConfigApplication Clz

--- mesh-config/src/main/java/com/cloud/mesh/config/ConfigApplication.java
+++ mesh-config/src/main/java/com/cloud/mesh/config/ConfigApplication.java
@@ -22,6 +22,6 @@ public class ConfigApplication {
         SpringApplication app = new SpringApplication(ConfigApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
-        log.info("BusinessApplication is running!");
+        log.info("ConfigApplication is running!");
     }
 }
