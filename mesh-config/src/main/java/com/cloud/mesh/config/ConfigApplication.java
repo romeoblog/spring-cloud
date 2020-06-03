92ee89f831c550cbcd2652254c556db311c14dc7
romeoblog <willluzheng@163.com>
2020-06-03 13:39:30 +0800

    Add ConfigApplication

---
+++ mesh-config/src/main/java/com/cloud/mesh/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.mesh.config;
+
+/**
+ * spring cloud config
+ *
+ * @author willlu.zheng
+ * @date 2020-06-03
+ */
+public class ConfigApplication {
+    public static void main(String[] args) {
+
+    }
+}
