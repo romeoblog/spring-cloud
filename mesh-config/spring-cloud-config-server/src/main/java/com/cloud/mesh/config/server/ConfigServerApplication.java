8390c9ed7c3fccb35b0b0a74f49bd3a7a48d7575
willlu.zheng <willluzheng@163.com>
2020-06-14 00:35:27 +0800

    Configuration server for the spring-cloud

---
+++ mesh-config/spring-cloud-config-server/src/main/java/com/cloud/mesh/config/server/ConfigServerApplication.java
@@ -0,0 +1,11 @@
+package com.cloud.mesh.config.server;
+
+/**
+ * Configuration server for the spring-cloud
+ *
+ * @author willlu.zheng
+ * @date 2020-06-14
+ */
+public class ConfigServerApplication {
+
+}
