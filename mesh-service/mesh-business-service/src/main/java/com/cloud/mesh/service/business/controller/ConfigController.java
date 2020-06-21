e444b20c762a2d237e6ac71e3a18b150f7cce8e6
willlu.zheng <willluzheng@163.com>
2020-06-21 15:55:14 +0800

    The BusinessApplication main method add @RefreshScope.

--- mesh-service/mesh-business-service/src/main/java/com/cloud/mesh/service/business/controller/ConfigController.java
+++ mesh-service/mesh-business-service/src/main/java/com/cloud/mesh/service/business/controller/ConfigController.java
@@ -18,7 +18,6 @@ package com.cloud.mesh.service.business.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +30,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/config")
-@RefreshScope
 @Api(value = "ConfigController", description = "Nacos测试")
 public class ConfigController {
 
