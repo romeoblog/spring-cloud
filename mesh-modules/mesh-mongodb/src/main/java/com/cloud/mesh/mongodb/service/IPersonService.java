2ca4774edf4c310083a21639c0950f32f35461d6
willlu.zheng <willluzheng@163.com>
2020-07-10 17:27:27 +0800

    Add mongodb interface api

---
+++ mesh-modules/mesh-mongodb/src/main/java/com/cloud/mesh/mongodb/service/IPersonService.java
@@ -0,0 +1,10 @@
+package com.cloud.mesh.mongodb.service;
+
+/**
+ * @author willlu.zheng
+ * @date 2020/7/10
+ */
+public interface IPersonService {
+
+
+}
---
+++ mesh-modules/mesh-mongodb/src/main/java/com/cloud/mesh/mongodb/service/impl/PersonServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cloud.mesh.mongodb.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+/**
+ * @author willlu.zheng
+ * @date 2020/7/10
+ */
+@Service
+public class PersonServiceImpl implements Serializable {
+
+
+}
